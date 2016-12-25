fc2b2850145f4a9103f14d626eadc415b0d6e7c2
Max Duan <dqn14@mails.tsinghua.edu.cn>
2016-12-25 17:31:56 +0800

    Enable continuously input on stdin

--- querylanguage/compiletest.cpp
+++ querylanguage/compiletest.cpp
@@ -14,12 +14,9 @@ SM_Manager *sm;
 QL_Manager *qm;
 
 int main(int argc, char* argv[]){
+  const int MAXBUF = 1024;
+  char buf[MAXBUF];
   FILE *fin;
-  if(argc < 2)
-    fin = stdin;
-  else
-    fin = fopen(argv[1], "r");
-  yyset_in(fin);
   MyBitMap::initConst();
   fm = new FileManager();
   bpm = new BufPageManager(fm);
@@ -27,8 +24,35 @@ int main(int argc, char* argv[]){
   ix = new IX_Manager(fm, bpm);
   sm = new SM_Manager(ix, rm);
   qm = new QL_Manager(sm, ix, rm);
-  yyparse();
-  fclose(fin);
+  if(argc < 2){
+    fin = stdin;
+    FILE *fout;
+    int flag = 1;
+    while(flag){
+      fout = fopen("temp.tmp", "w");
+      while(gets(buf) != NULL){
+        if(strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0){
+          flag = 0;
+          break;
+        }
+        else{
+          fprintf(fout, "%s\n", buf);
+        }
+      }
+      fclose(fout);
+      system("cat temp.tmp");
+      fin = fopen("temp.tmp", "r");
+      yyset_in(fin);
+      yyparse();
+      fclose(fin);
+    }
+  }
+  else{
+    fin = fopen(argv[1], "r");
+    yyset_in(fin);
+    yyparse();
+    fclose(fin);
+  }
   delete fm;
   delete bpm;
   delete rm;
