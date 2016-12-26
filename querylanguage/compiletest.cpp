fa3ffd9d6f31968223bdf2db30488a85dd74d18e
岑宇阔 <cenyk14@mails.tsinghua.edu.cn>
2016-12-26 15:25:46 +0800

    Recover compiletest.cpp

--- querylanguage/compiletest.cpp
+++ querylanguage/compiletest.cpp
@@ -14,9 +14,51 @@ SM_Manager *sm;
 QL_Manager *qm;
 
 int main(int argc, char* argv[]){
-  const int MAXBUF = 1024;
-  char buf[MAXBUF];
+  // const int MAXBUF = 1024;
+  // char buf[MAXBUF];
+  // FILE *fin;
+  // MyBitMap::initConst();
+  // fm = new FileManager();
+  // bpm = new BufPageManager(fm);
+  // rm = new RM_Manager(fm, bpm);
+  // ix = new IX_Manager(fm, bpm);
+  // sm = new SM_Manager(ix, rm);
+  // qm = new QL_Manager(sm, ix, rm);
+  // if(argc < 2){
+  //   fin = stdin;
+  //   FILE *fout;
+  //   int flag = 1;
+  //   while(flag){
+  //     fout = fopen("temp.tmp", "w");
+  //     while(gets(buf) != NULL){
+  //       if(strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0){
+  //         flag = 0;
+  //         break;
+  //       }
+  //       else{
+  //         fprintf(fout, "%s\n", buf);
+  //       }
+  //     }
+  //     fclose(fout);
+  //     system("cat temp.tmp");
+  //     fin = fopen("temp.tmp", "r");
+  //     yyset_in(fin);
+  //     yyparse();
+  //     fclose(fin);
+  //   }
+  // }
+  // else{
+  //   fin = fopen(argv[1], "r");
+  //   yyset_in(fin);
+  //   yyparse();
+  //   fclose(fin);
+  // }
   FILE *fin;
+  if(argc < 2)
+    fin = stdin;
+  else
+    fin = fopen(argv[1], "r");
+  yyset_in(fin);
   MyBitMap::initConst();
   fm = new FileManager();
   bpm = new BufPageManager(fm);
@@ -24,35 +66,8 @@ int main(int argc, char* argv[]){
   ix = new IX_Manager(fm, bpm);
   sm = new SM_Manager(ix, rm);
   qm = new QL_Manager(sm, ix, rm);
-  if(argc < 2){
-    fin = stdin;
-    FILE *fout;
-    int flag = 1;
-    while(flag){
-      fout = fopen("temp.tmp", "w");
-      while(gets(buf) != NULL){
-        if(strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0){
-          flag = 0;
-          break;
-        }
-        else{
-          fprintf(fout, "%s\n", buf);
-        }
-      }
-      fclose(fout);
-      system("cat temp.tmp");
-      fin = fopen("temp.tmp", "r");
-      yyset_in(fin);
-      yyparse();
-      fclose(fin);
-    }
-  }
-  else{
-    fin = fopen(argv[1], "r");
-    yyset_in(fin);
-    yyparse();
-    fclose(fin);
-  }
+  yyparse();
+  fclose(fin);
   delete fm;
   delete bpm;
   delete rm;
